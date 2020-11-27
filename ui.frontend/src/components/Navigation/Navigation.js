66a75c57844d52f0040b6280c68f565d1915c50e
Sasha Romanyshyn <sromanyshyn@tmg.io>
2020-11-27 13:55:33 +0200

    Add navigation component file

---
+++ ui.frontend/src/components/Navigation/Navigation.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { MapTo } from '@adobe/aem-react-editable-components';
+
+const NavigationEditConfig = {
+    emptyLabel: 'Navigation',
+
+    isEmpty: function(props) {
+        return !props || !props.navigationRoot || props.navigationRoot.trim().length < 1;
+    }
+};
+
+class Navigation extends React.Component {
+
+    render() {
+        console.log(this.props);
+        return (
+          <div>
+              <p>Navigation!</p>
+          </div>
+        );
+    }
+}
+
+export default MapTo('bbb/components/navigation')(
+    Navigation,
+    NavigationEditConfig
+);
